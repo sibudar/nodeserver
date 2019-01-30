15d5c30e64840d40dcb7743980a5207dfd7d609c
academy_intern <academy_intern@academy-interns-MacBook-Pro.local>
2019-01-30 15:06:55 +0200

    Get all clients API

--- routes/applications.js
+++ routes/applications.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const connection = require('../connectionDB/mysql');
 const router = express.Router();
+const app = express();
 
-router.get('/', (req, res, next) =>{
-    res.status(200).json({
-        message :'Get all applications'
-    })
-  });
+// router.get('/', (req, res, next) => {
+//     res.status(200).json({
+//         message: 'Get all applications'
+//     })
+// });
 
-  router.get('/nonny', (req, res, next) =>{
+router.get('/nonny', (req, res, next) => {
     res.status(200).json({
-        message :'added nonny'
+        message: 'added nonny'
     })
-  });
-module.exports=router;
+});
+
+//Get all applications
+router.get('/', function (req, resp) {
+
+    console.log('Devon')
+    connection.query("SELECT * FROM applications WHERE active =1", function (error, rows, fields) {
+        if (error) {
+            console.log('Error in the query');
+        } else {
+            resp.json(rows);
+        }
+
+    })
+
+});
+
+module.exports = router;
