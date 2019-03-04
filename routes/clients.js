9c68a115ecb8886e8fee07ff7687d88d301348d1
nonny <nonhlanhlamkhonza5@gmail.com>
2019-03-04 16:37:15 +0200

    Added comments for client route

--- routes/clients.js
+++ routes/clients.js
@@ -1,37 +1,60 @@
+/*
+Author: Nonny
+
+Client Router
+
+We handle all front-end http requests in this file 
+and let client-controller to do all the 
+validations and some other logics
+
+*/
+
+
+
 const express = require('express');
-const ClientCtr = require('../controller/client-controller');
+const ClientCtr = require('../controller/client-controller'); //controller
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-//console.log("uclient uya runner")
 
-  router.get('/nonny', (req, res, next) =>{
-    res.status(200).json({
-        message :'added nonny'
-    })
-  });
 
-//clients login
+/* 
+
+clients login
+
+required parameters(email,password)
+returns: login status as string 
+
+*/
 router.post('/login', async function(req, res){
-    // console.log("req")
-    var email =  req.body.email;
-    var password = req.body.password;
-    var result = await ClientCtr.Login(email,password);
+    
+    var email =  req.body.email; // email parameter
+    var password = req.body.password; //password parameter
 
-    res.send(result);
+    var result = await ClientCtr.Login(email,password); //passing values to the Controller
+
+    res.send(result); //send back to the front-end
    
 })
 
-      //Insert clients
-      router.post('/add-clients',async function(req, res) {
+
+
+/* 
+
+Insert clients
+
+required parameters (firstname,lastname,organisation,password,active,adminID)
+returns status as a string
+*/
+router.post('/add-clients',async function(req, res) {
         
-    var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var organization = req.body.organization;
-    var password = bcrypt.hashSync(req.body.password,10);
-    var email = req.body.email;
-    var active = req.body.active;
-    var adminID = req.body.adminID;
+    var firstname = req.body.firstname; //firstname : String
+    var lastname = req.body.lastname; //lastname : string
+    var organization = req.body.organization; //organisation : string
+    var password = bcrypt.hashSync(req.body.password,10); //encrypted password : string
+    var email = req.body.email; //email : string
+    var active = req.body.active; // actvie : integer
+    var adminID = req.body.adminID; //adminID : integer
 
     var result = await ClientCtr.addClient(firstname, lastname, organization, password,email, active, adminID);
 
@@ -43,23 +66,36 @@ router.post('/login', async function(req, res){
  
  //admin displaying all clients
 router.get('/display-clients',async function(req, res)  {
-    var result = await ClientCtr.displayClient();
+    var result = await ClientCtr.displayClient(); //from client-controller
     res.send(result);
         
 }) 
 
 
-//display single client  
+/* 
+
+display single client
+
+required parameter (id)
+returns an array of objects
+*/
 router.get('/getSingle-client/:id',async function(req, res){
  
-      var id = (req.params.id);
-      var result = await ClientCtr.singleClient(id);
+      var id = (req.params.id); //id : string
+      var result = await ClientCtr.singleClient(id); //from client controller
 
     res.send(result[0]);
 
 })
           
-//delete clients
+/*
+
+delete clients
+
+required parameters (id)
+returns status as a string
+
+*/
 router.post('/delete-clients',async function(req, res){
       var id = req.body.id;
       var result = await ClientCtr.deleteClient(id);
@@ -69,7 +105,13 @@ router.post('/delete-clients',async function(req, res){
     })
 
 
-    // activating clients
+/* 
+
+activating clients
+
+required parameter(id)
+returns a status as string
+*/
 router.post('/activate-clients', async function(req, res){
       var id = req.body.id;
 
@@ -80,20 +122,23 @@ router.post('/activate-clients', async function(req, res){
   }) 
   
 
-//update clients
+/* 
+
+update clients
+required parameters (id, lastname,organisation,email)
+returns a status as a string
+*/
 router.post('/update-clients',async function(req, res){
-  var id = req.body.id;
-  var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var organization = req.body.organization;
-    var email = req.body.email;
+  var id = req.body.id; //id : integer
+  var firstname = req.body.firstname; //firstname : string
+    var lastname = req.body.lastname;//lastname : string
+    var organization = req.body.organization; //organization : string
+    var email = req.body.email; //email : string
 
     var result = await ClientCtr.updateClient(id,firstname, lastname, organization, email);
 
     res.send(result[0]);
     
-  //var sql = "UPDATE clients SET firstname='"+firstname+"',lastname='"+lastname+"',organization='"+organization+"',password='"+password+"',email='"+email+"',active="+active +" WHERE id="+mysql.escape(id);
-  //var sql = `CALL sp_updateClients ('${id = id}','${firstname = firstname}','${lastname = lastname}','${organization = organization}','${email = email}')`;
 })
 
 
